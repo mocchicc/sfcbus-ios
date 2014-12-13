3e1f572d1a9d53eea3e1c2c1611d00976e9b6b0b
Hiroyuki Tsuruda <mocchicc@me.com>
2014-12-13 11:18:39 +0900

    Initial Commit

---
+++ sfcbus-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sfcbus-ios
+//
+//  Created by mocchicc on 2014/12/13.
+//  Copyright (c) 2014年 mocchicc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
